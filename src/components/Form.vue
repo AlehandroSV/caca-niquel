2e9ebe05ccf1ae792020a028cb052a4d59d77f49
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-30 10:13:07 -0300

    styles: Ajustando tamanho input

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -27,7 +27,7 @@
       <template v-else-if="!field.salvarDados">
         <div class="my-1 flex flex-col">
           <div
-            class="flex border border-black dark:border-white items-center gap-3 py-1 px-2 w-full text-sm rounded-full focus-within:border-primary focus-within:ring-1 ring-secondary"
+            class="flex border border-black dark:border-white items-center gap-3 py-2 px-2 w-full text-sm rounded-full focus-within:border-primary focus-within:ring-1 ring-secondary"
             v-if="field.type !== 'submit' && !field.salvarDados"
           >
             <input
